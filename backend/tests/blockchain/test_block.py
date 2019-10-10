5ec3f51d17f03ea41c5ea3e65b07dbd52013e3b5
David Katz <15Dkatz@shcp.edu>
2019-10-10 08:28:33 -0700

    Improve mine_block

--- backend/tests/blockchain/test_block.py
+++ backend/tests/blockchain/test_block.py
@@ -8,6 +8,7 @@ def test_mine_block():
     assert isinstance(block, Block)
     assert block.data == data
     assert block.last_hash == last_block.hash
+    assert block.hash[0:block.difficulty] == '0' * block.difficulty
 
 def test_genesis():
     genesis = Block.genesis()
