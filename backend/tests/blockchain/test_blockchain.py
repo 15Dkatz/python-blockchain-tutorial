5dfdcabf9aabd589de5764463e0aef75f1ca06c8
David Katz <15Dkatz@shcp.edu>
2019-10-10 08:23:54 -0700

    Complete section 2: Test the application

---
+++ backend/tests/blockchain/test_blockchain.py
@@ -0,0 +1,13 @@
+from backend.blockchain.blockchain import Blockchain
+from backend.blockchain.block import GENESIS_DATA
+
+def test_blockchain_instance():
+    blockchain = Blockchain()
+    assert blockchain.chain[0].hash == GENESIS_DATA['hash']
+
+def test_add_block():
+    blockchain = Blockchain()
+    data = 'test-data'
+    blockchain.add_block(data)
+
+    assert blockchain.chain[-1].data == data
