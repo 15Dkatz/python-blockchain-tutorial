a6142eb9dacdc5f71e213456acc52851c4124060
David Katz <15Dkatz@shcp.edu>
2019-10-10 21:34:59 -0700

    Validate a block and test is_valid_block

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -1,6 +1,7 @@
 import time
 
 from backend.util.crypto_hash import crypto_hash
+from backend.util.hex_to_binary import hex_to_binary
 from backend.config import MINE_RATE
 
 GENESIS_DATA = {
@@ -48,7 +49,7 @@ class Block:
         nonce = 0
         hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
-        while hash[0:difficulty] != '0' * difficulty:
+        while hex_to_binary(hash)[0:difficulty] != '0' * difficulty:
             nonce += 1
             timestamp = time.time_ns()
             difficulty = Block.adjust_difficulty(last_block, timestamp)
@@ -78,10 +79,44 @@ class Block:
 
         return 1
 
+    @staticmethod
+    def is_valid_block(last_block, block):
+        """
+        Validate block by enforcing the following rules:
+          - the block must have the proper last_hash reference
+          - the block must meet the proof of work requirement
+          - the difficulty must only adjust by 1
+          - the block hash must be a valid combination of the block fields
+        """
+        if block.last_hash != last_block.hash:
+            raise Exception('The block last_hash must be correct')
+
+        if hex_to_binary(block.hash)[0:block.difficulty] != '0' * block.difficulty:
+            raise Exception('The proof of work requirement was not met')
+
+        if abs(last_block.difficulty - block.difficulty) > 1:
+            raise Exception('The block difficulty must only adjust by 1')
+
+        reconstructed_hash = crypto_hash(
+            block.timestamp,
+            block.last_hash,
+            block.data,
+            block.nonce,
+            block.difficulty
+        )
+
+        if block.hash != reconstructed_hash:
+            raise Exception('The block hash must be correct')
+
 def main():
     genesis_block = Block.genesis()
-    block = Block.mine_block(genesis_block, 'foo')
-    print(block)
+    bad_block = Block.mine_block(genesis_block, 'foo')
+    bad_block.last_hash = 'evil_data'
+
+    try:
+        Block.is_valid_block(genesis_block, bad_block)
+    except Exception as e:
+        print(f'is_valid_block: {e}')
 
 if __name__ == '__main__':
     main()
--- backend/tests/blockchain/test_block.py
+++ backend/tests/blockchain/test_block.py
@@ -1,3 +1,4 @@
+import pytest
 import time
 
 from backend.blockchain.block import Block, GENESIS_DATA
@@ -48,3 +49,40 @@ def test_mined_block_difficulty_limits_at_1():
     mined_block = Block.mine_block(last_block, 'bar')
 
     assert mined_block.difficulty == 1
+
+@pytest.fixture
+def last_block():
+	return Block.genesis()
+
+@pytest.fixture
+def block(last_block):
+	return Block.mine_block(last_block, 'test_data')
+
+def test_is_valid_block(last_block, block):
+	Block.is_valid_block(last_block, block)
+
+def test_is_valid_block_bad_last_hash(last_block, block):
+	block.last_hash = 'evil_last_hash'
+
+	with pytest.raises(Exception, match='last_hash must be correct'):
+		Block.is_valid_block(last_block, block)
+
+def test_is_valid_block_bad_proof_of_work(last_block, block):
+	block.hash = 'fff'
+
+	with pytest.raises(Exception, match='proof of work requirement was not met'):
+		Block.is_valid_block(last_block, block)
+
+def test_is_valid_block_jumped_difficulty(last_block, block):
+	jumped_difficulty = 10
+	block.difficulty = jumped_difficulty
+	block.hash = f'{"0" * jumped_difficulty}111abc'
+
+	with pytest.raises(Exception, match='difficulty must only adjust by 1'):
+		Block.is_valid_block(last_block, block)
+
+def test_is_valid_block_bad_block_hash(last_block, block):
+	block.hash = '0000000000000000bbbabc'
+
+	with pytest.raises(Exception, match='block hash must be correct'):
+		Block.is_valid_block(last_block, block)
