f5ed4dc2273bd4f278ce756cce8bfd83313d2f87
David Katz <15Dkatz@shcp.edu>
2019-10-15 20:41:07 -0700

    Complete section: Introduction to Web Development, React, and React Hooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Joke from './Joke';
 
 function App() {
+  const [userQuery, setUserQuery] = useState('');
+
+  const updateUserQuery = event => {
+    console.log('userQuery', userQuery);
+    setUserQuery(event.target.value); 
+  }
+
+  const searchQuery = () => {
+    window.open(`https://google.com/search?q=${userQuery}`);
+  }
+
+  const handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      searchQuery();
+    } 
+  }
+
   return (
-    <div className="App">App</div>
+    <div>
+      <input value={userQuery} onChange={updateUserQuery} onKeyPress={handleKeyPress} />
+      <button onClick={searchQuery}>Search</button>
+      <div>{userQuery}</div>
+      <hr />
+      <Joke />
+    </div>
   );
 }
 
