2e35923da3a8ee23f82ada09ea4e453be923d956
itsmeAman03 <aman.kr,ak03@gmail.com>
2022-06-08 09:59:37 +0530

    .

--- backend/util/crypto_hash.py
+++ backend/util/crypto_hash.py
@@ -5,8 +5,9 @@ def crypto_hash(*args):
     """
     Return a sha-256 hash of the given arguments.
     """
-    stringified_args = sorted(map(lambda data: json.dumps(data), args))
-    joined_data = ''.join(stringified_args)
+    # stringified_args = sorted(map(lambda data: json.dumps(data), args))
+    stringified_args=map(dumps(data),args)
+    joined_data = '^'.join(stringified_args)
 
     return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
 
